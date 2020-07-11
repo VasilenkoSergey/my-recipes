1918b4ad7bc308e41200c2f12dde5f114bf385bb
Sergey Vasilenko <sergey@vasilenko.io>
2020-07-11 23:40:25 +0300

    Add navigation component

--- app/src/main/java/io/vasilenko/myrecipes/presentation/MainActivity.kt
+++ app/src/main/java/io/vasilenko/myrecipes/presentation/MainActivity.kt
@@ -2,21 +2,48 @@ package io.vasilenko.myrecipes.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.vasilenko.myrecipes.R
 import io.vasilenko.myrecipes.databinding.ActivityMainBinding
-import io.vasilenko.myrecipes.presentation.catalog.CatalogFragment
+import io.vasilenko.myrecipes.presentation.common.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.container, CatalogFragment())
-                .commitAllowingStateLoss()
+            setupBottomNavigationBar()
         }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        val navGraphIds = listOf(
+            R.navigation.catalog_navigation,
+            R.navigation.create_navigation,
+            R.navigation.more_navigation
+        )
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.fragmentContainer,
+            intent = intent
+        )
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 }
