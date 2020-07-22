36cfc5ebd30f84bb5fd90c265341c01a1392aa30
Sergey Vasilenko <sergey@vasilenko.io>
2020-07-22 12:39:21 +0300

    Create placeholder for empty catalog items

--- app/src/main/java/io/vasilenko/myrecipes/presentation/catalog/CatalogViewModel.kt
+++ app/src/main/java/io/vasilenko/myrecipes/presentation/catalog/CatalogViewModel.kt
@@ -7,7 +7,10 @@ import androidx.lifecycle.viewModelScope
 import io.vasilenko.myrecipes.domain.usecase.LoadAllCategoriesUseCase
 import io.vasilenko.myrecipes.domain.usecase.LoadAllRecipesUseCase
 import io.vasilenko.myrecipes.presentation.common.ListItem
-import io.vasilenko.myrecipes.presentation.model.*
+import io.vasilenko.myrecipes.presentation.model.CatalogCategoryItem
+import io.vasilenko.myrecipes.presentation.model.CatalogEmptyItem
+import io.vasilenko.myrecipes.presentation.model.CatalogGroupItem
+import io.vasilenko.myrecipes.presentation.model.CatalogRecipeItem
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -28,18 +31,14 @@ class CatalogViewModel @Inject constructor(
     }
 
     private suspend fun getItems(): List<ListItem> {
-        val favoriteRecipes = listOf(
-            Recipe(1L, "Название", ""),
-            Recipe(2L, "Название", ""),
-            Recipe(3L, "Название", "")
-        ).map {
-            CatalogFavoriteRecipeItem(
-                id = it.id,
-                title = it.title,
-                image = it.image
-            )
-        }
 
+        val emptyFavorites = CatalogEmptyItem("Список избранного пуст")
+        val favoriteRecipes = emptyList<ListItem>()
+        val favoriteItems: List<ListItem>
+        favoriteItems =
+            if (favoriteRecipes.isNotEmpty()) favoriteRecipes else listOf(emptyFavorites)
+
+        val emptyRecipes = CatalogEmptyItem("Рецепты отсутствуют")
         val recipes = loadAllRecipesUseCase.execute()
             .map {
                 CatalogRecipeItem(
@@ -48,7 +47,10 @@ class CatalogViewModel @Inject constructor(
                     image = ""
                 )
             }
+        val recipeItems: List<ListItem>
+        recipeItems = if (recipes.isNotEmpty()) recipes else listOf(emptyRecipes)
 
+        val emptyCategories = CatalogEmptyItem("Категории не добавлены")
         val categories = loadAllCategoriesUseCase.execute()
             .map {
                 CatalogCategoryItem(
@@ -57,19 +59,21 @@ class CatalogViewModel @Inject constructor(
                     image = ""
                 )
             }
+        val categoryItems: List<ListItem>
+        categoryItems = if (categories.isNotEmpty()) categories else listOf(emptyCategories)
 
         return listOf(
             CatalogGroupItem(
                 "Избранное",
-                favoriteRecipes
+                favoriteItems
             ),
             CatalogGroupItem(
                 "Рецепты",
-                recipes
+                recipeItems
             ),
             CatalogGroupItem(
                 "Категории",
-                categories
+                categoryItems
             )
         )
     }
