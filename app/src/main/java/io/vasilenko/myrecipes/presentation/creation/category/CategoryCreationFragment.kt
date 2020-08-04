4a1332176cd72f58bbf22b3c6aa76f65eaa670d4
Sergey Vasilenko <sergey@vasilenko.io>
2020-08-05 01:07:28 +0300

    Creation cancel dialog implementation

--- app/src/main/java/io/vasilenko/myrecipes/presentation/creation/category/CategoryCreationFragment.kt
+++ app/src/main/java/io/vasilenko/myrecipes/presentation/creation/category/CategoryCreationFragment.kt
@@ -2,8 +2,12 @@ package io.vasilenko.myrecipes.presentation.creation.category
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import io.vasilenko.myrecipes.R
 import io.vasilenko.myrecipes.databinding.FragmentCreationCategoryBinding
 import io.vasilenko.myrecipes.di.component.CategoryCreationComponent
@@ -16,15 +20,19 @@ class CategoryCreationFragment : Fragment(R.layout.fragment_creation_category) {
     private val component by lazy { CategoryCreationComponent.create() }
     private val viewModel by viewModels<CategoryCreationViewModel> { component.viewModelFactory() }
 
+    private lateinit var navController: NavController
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
         setupView()
+        setupBackPress()
     }
 
     private fun setupView() {
         with(binding) {
             toolbar.title = getString(R.string.create_category_title)
-            toolbar.setNavigationOnClickListener { close() }
+            toolbar.setNavigationOnClickListener { requireActivity().onBackPressed() }
         }
 
         val createBtn = binding.createBtn
@@ -37,7 +45,24 @@ class CategoryCreationFragment : Fragment(R.layout.fragment_creation_category) {
         }
     }
 
+    private fun setupBackPress() {
+        activity?.onBackPressedDispatcher?.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    navController.navigate(R.id.action_categoryCreationFragment_to_creation_cancel_dialog)
+                }
+            })
+
+        navController.currentBackStackEntry?.savedStateHandle?.getLiveData<Boolean>(CANCELLED_FLAG)
+            ?.observe(viewLifecycleOwner, Observer { if (it) close() })
+    }
+
     private fun close() {
-        requireActivity().onBackPressed()
+        navController.popBackStack()
+    }
+
+    companion object {
+        const val CANCELLED_FLAG = "is_cancelled"
     }
 }
--- app/src/main/java/io/vasilenko/myrecipes/presentation/creation/recipe/RecipeCreationFragment.kt
+++ app/src/main/java/io/vasilenko/myrecipes/presentation/creation/recipe/RecipeCreationFragment.kt
@@ -2,8 +2,12 @@ package io.vasilenko.myrecipes.presentation.creation.recipe
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import io.vasilenko.myrecipes.R
 import io.vasilenko.myrecipes.databinding.FragmentCreationRecipeBinding
 import io.vasilenko.myrecipes.di.component.RecipeCreationComponent
@@ -16,9 +20,13 @@ class RecipeCreationFragment : Fragment(R.layout.fragment_creation_recipe) {
     private val component by lazy { RecipeCreationComponent.create() }
     private val viewModel by viewModels<RecipeCreationViewModel> { component.viewModelFactory() }
 
+    private lateinit var navController: NavController
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
         setupView()
+        setupBackPress()
     }
 
     private fun setupView() {
@@ -37,7 +45,24 @@ class RecipeCreationFragment : Fragment(R.layout.fragment_creation_recipe) {
         }
     }
 
+    private fun setupBackPress() {
+        activity?.onBackPressedDispatcher?.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    navController.navigate(R.id.action_recipeCreationFragment_to_creation_cancel_dialog)
+                }
+            })
+
+        navController.currentBackStackEntry?.savedStateHandle?.getLiveData<Boolean>(CANCELLED_FLAG)
+            ?.observe(viewLifecycleOwner, Observer { if (it) close() })
+    }
+
     private fun close() {
-        requireActivity().onBackPressed()
+        navController.popBackStack()
+    }
+
+    companion object {
+        const val CANCELLED_FLAG = "is_cancelled"
     }
 }
