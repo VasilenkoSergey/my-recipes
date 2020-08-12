cf28b77f17f82c3a3bbfc19dfe2372325481b587
Sergey Vasilenko <sergey@vasilenko.io>
2020-08-13 01:52:29 +0300

    Add new items to bottom navigation

---
+++ app/src/main/java/io/vasilenko/myrecipes/presentation/cooking/CookingFragment.kt
@@ -0,0 +1,7 @@
+package io.vasilenko.myrecipes.presentation.cooking
+
+import androidx.fragment.app.Fragment
+import io.vasilenko.myrecipes.R
+
+class CookingFragment: Fragment(R.layout.fragment_cooking) {
+}
